583208b666e3f975ea71a92dc62e150e88bb0a12
hazedrops <hazedrops@hotmail.com>
2021-08-23 23:16:13 -0700

    Added add items & clear cart functions

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const productsDOM = document.querySelector('.products-center');
 // cart
 let cart = [];
 
+// buttons
+let buttonsDOM = [];
+
 // getting the products
 class Products {
   async getProducts() {
@@ -48,7 +51,7 @@ class UI {
             <img src=${product.image} alt="product" class="product-img">
             <button class="bag-btn" data-id=${product.id}>
               <i class="fas fa-shopping-cart"></i>
-              add to bag
+              add to cart
             </button>
           </div>
           <h3>${product.title}</h3>
@@ -60,17 +63,180 @@ class UI {
 
     productsDOM.innerHTML = result;
   }
+
+  getBagButtons() {
+    // turn the bag-btn node list into an array by using [...]
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+
+      if(inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } 
+      
+      button.addEventListener("click", event => {
+        event.target.innerText = "In Cart";
+        event.target.disabled = true;
+
+        // Getting the item from the local storage 
+        // And add the 'amount' property and make the data into cartItem object
+        let cartItem = {...Storage.getProduct(id), amount:1 };
+
+        // add product to the cart
+        cart = [...cart, cartItem];
+
+        // save the cart into the local storage
+        Storage.saveCart(cart);
+
+        // set cart values
+        this.setCartValues(cart);
+
+        // display cart item
+        this.addCartItem(cartItem);
+
+        // show the cart
+        this.showCart();
+      });
+    });
+  }
+
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    })
+
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+
+  addCartItem(item) {
+    const div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `
+      <img src=${item.image} alt="product">
+          <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+          </div>
+          <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+          </div>
+    `;
+    cartContent.appendChild(div);
+  }
+
+  showCart() {
+    cartOverlay.classList.add('transparentBcg');
+    cartDOM.classList.add('showCart');
+  }
+
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener('click', this.showCart);
+    closeCartBtn.addEventListener('click', this.hideCart);
+  }
+
+  populateCart(cart) {
+    cart.forEach(item =>this.addCartItem(item));
+  }
+
+  hideCart() {
+    cartOverlay.classList.remove('transparentBcg');
+    cartDOM.classList.remove('showCart');
+  }
+
+  cartLogic() {
+    // clearCartBtn.addEventListener("click", this.clearCart());
+    // 'this' in this case is a button, not the UI
+    //
+    // When you need to access the method in the class check which target is the 'this' pointing to
+    clearCartBtn.addEventListener("click", () => {
+      this.clearCart();
+    });      
+
+    // cart functionality
+
+  }
+
+  clearCart() {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));   
+    console.log(cartContent.children);
+    
+    // keep removing cart content until there's no more items in cart
+    while(cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+
+    this.hideCart();
+  }
+
+  removeItem(id) {
+    cart = cart.filter(item => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `
+      <i class="fas fa-shopping-cart"></i>add to cart
+    `;
+  }
+
+  getSingleButton(id) {
+    // find and returns a button with a certain id from the buttonsDOM
+    return buttonsDOM.find(button => button.dataset.id === id)
+  }
 }
 
-// local storage
+// local storage - to speed up bringing data about products
 class Storage{
-  
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === id);
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  static getCart() {
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
+  // setup app
+  ui.setupAPP();
+
   // get all products
-  products.getProducts().then(products => ui.displayProducts(products));
+  // 1st then : display products on the page & save the product data into the localstorage
+  // 2nd then : get buttons of the product images after all the elements are loaded
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+  }).then(() => {
+    ui.getBagButtons();
+    ui.cartLogic();
+  });
 });
